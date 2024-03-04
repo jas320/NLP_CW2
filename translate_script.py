5ed1688ea16126ce63472fc3854a806636381e66
jas320 <jas320@ic.ac.uk>
2024-03-04 03:30:40 +0000

    try adding script again

--- translate_script.py
+++ translate_script.py
@@ -16,18 +16,19 @@ def translate_group(text_list, label_list):
     translators_to = [GoogleTranslator(source='en', target=x) for x in choices]
     translators_from = [GoogleTranslator(source=x, target='en') for x in choices]
     for text, label in zip(text_list, label_list):
+        n_items = 3
         aug_text_list.append(text)
-        for i in range(3):
+        for i in range(n_items):
             new_text = [translators_from[i].translate(translators_to[i].translate(text))]
             aug_text_list.extend(new_text)
         aug_label_list.append(label)
-        aug_label_list.extend([label] * len(new_text))
+        aug_label_list.extend([label] * n_items)
     return aug_text_list, aug_label_list
 
 
 text, labels = read_list_from_file("full_data_text_list.pkl"), read_list_from_file("full_data_label_list.pkl")
 res1, res2 = translate_group(text, labels)
 # [print(x) for x in res1]
-write_list_to_file(res1, "3_lang_translate_text.txt")
-write_list_to_file(res2, "3_lang_labels.txt")
+write_list_to_file(res1, "3_lang_translate_text.pkl")
+write_list_to_file(res2, "3_lang_labels.pkl")
 
